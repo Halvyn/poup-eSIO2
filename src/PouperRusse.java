0f613ca012dfbfb36fcb0396a44d9ead0ebfd1d0
Abder-Rahmane <you@example.com>
2021-09-23 13:22:37 +0200

    Ajout des constructeur

--- src/PouperRusse.java
+++ src/PouperRusse.java
@@ -3,4 +3,10 @@ public class PouperRusse {
     public int taille;
     private PouperRusse p;
     public boolean ouverte;
+
+    public PouperRusse(int taille, PouperRusse p, boolean ouverte) {
+        this.taille = taille;
+        this.p = p;
+        this.ouverte = ouverte;
+    }
 }
